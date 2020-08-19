c5d18dbc67cd4708cc1ea1b5c66acbec23221a2c
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-08-18 22:31:24 -0700

    added styled components and styled authlayout and signup page

--- web/src/layouts/AuthenticationLayout/AuthenticationLayout.js
+++ web/src/layouts/AuthenticationLayout/AuthenticationLayout.js
@@ -1,12 +1,54 @@
+import { Link, routes } from '@redwoodjs/router'
+import styled from 'styled-components'
+
 const AuthenticationLayout = ({ children }) => {
-  return (
-  	<>
-  		<div className="leftside">
-        	<a href="#">microfails</a>
-        	<p>Discover the new opportunities to hopefully better yourself</p>
-      	</div>
-  		<main>{children}</main>
-  	</>
+
+	const MainContainer = styled.div`
+		display: flex;
+		flex-direction: row;
+		overflow: hidden;
+	`
+
+	const AuthSideBar = styled.section`
+    	background: #4d4891;
+    	color: #fff;
+    	height: 100vh;
+    	width: 32.1rem;
+  	`
+
+  	const SidebarContent = styled.div`
+	    display: flex;
+	    flex-direction: column;
+	    justify-content: space-between;
+    `
+
+    const Artwork = styled.div`
+    	display: flex;
+    	flex-direction: column;
+    	justify-content flex-end;
+    	flex-grow: 1;
+    `
+
+    const Header = styled.header`
+    	padding: 4rem 4rem 5rem;
+    	max-width: 100%;
+    	margin: 0;
+    	text-align: left;
+    `
+
+	return (
+		<MainContainer>
+			<AuthSideBar>
+				<SidebarContent>
+					<Header>
+						<Link to={routes.home()} style={{ fontFamily: 'Gilbert color', fontSize: '5rem' }}>microfails</Link>
+		    			<h1>Fail. Together. 🤦‍♀️</h1>
+					</Header>
+					<Artwork />
+				</SidebarContent>
+	    	</AuthSideBar>
+			<main>{children}</main>
+		</MainContainer>
   )
 }
 
--- web/src/pages/SignupPage/SignupPage.js
+++ web/src/pages/SignupPage/SignupPage.js
@@ -1,14 +1,48 @@
 import { Link, routes } from '@redwoodjs/router'
 import AuthenticationLayout from 'src/layouts/AuthenticationLayout'
+import styled from 'styled-components'
 
 const SignupPage = () => {
+
+  const Content = styled.section`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    overflow: auto;
+    width: 30rem;
+  `
+
+  const AuthNav = styled.nav`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-grow: 0;
+    padding: 30px 30px 0;
+  `
+
+  const AuthLink = styled.p`
+    margin: 0;
+    flex-grow: 1;
+    text-align: right;
+  `
+
+  const AuthContent = styled.div`
+    padding: 30px 60px 0;
+    margin: 0;
+  `
+
+
   return (
     <AuthenticationLayout>
-      <p>Already a member?</p><Link to={routes.signin()}>Sign In</Link>
-      <div className="rightside">
-        <h3>Sign up to Microfails</h3>
-        <button>Create Account</button>
-      </div>
+      <Content>
+        <AuthNav>
+          <p>Already a member? <Link to={routes.signin()}>Sign In</Link></p>
+        </AuthNav>
+        <AuthContent>
+          <h3>Sign up to Microfails</h3>
+          <button>Create Account</button>
+        </AuthContent>
+      </Content>
     </AuthenticationLayout>
   )
 }
