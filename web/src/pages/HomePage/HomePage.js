51db274c8b92d272d05ddb9f6809f1428463ae00
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-08-20 11:37:51 -0700

    changed homepage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,17 +1,16 @@
 import { Link, routes } from '@redwoodjs/router'
+import styled from 'styled-components'
+import { Button } from 'src/styled'
 
 const HomePage = () => {
+
+
+
   return (
     <>
       <div className="banner">
         <h1>Build new habits and celebrate the fails</h1>
         <p>Microfails is the leading destination to find and create new habit building opportunities</p>
-        <button>
-          <Link to={routes.signup()}>
-            Sign up
-          </Link>
-        </button>
-        <img src="#" />
       </div>
     </>
   )
