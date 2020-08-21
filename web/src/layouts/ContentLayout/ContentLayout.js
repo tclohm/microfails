00bce5373a85601cda6eebad4dc80ecd99ba0fef
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-08-20 17:46:00 -0700

    modified buttons on homepage and sigin/signup

--- web/src/layouts/ContentLayout/ContentLayout.js
+++ web/src/layouts/ContentLayout/ContentLayout.js
@@ -22,20 +22,30 @@ const ContentLayout = ({ children }) => {
   		<Header>
   			<h3 style={{ margin: '1rem 0 1rem 2rem', fontFamily: 'Gilbert' }}>microfails</h3>
   			<Nav>
-  				<Button
-  					inputBGColor='transparent'
-  					inputBorder='transparent'
-  					inputHoverColor='transparent'
-  				>
-  					<Link to={routes.signin()} className='auth'>Sign in</Link>
-  				</Button>
-  				<Button
-  					inputHeight='2.5rem'
-  					inputBGColor='#FFC843'
-  					inputBorder='#FFC843'
-  				>
-  					<Link to={routes.signup()} className="auth">Sign up</Link>
-  				</Button>
+          <Link to={routes.signin()} className='auth'>
+    				<Button
+              inputHeight='2.5rem'
+              inputWidth='5rem'
+              inputColor='gray'
+    					inputBGColor='transparent'
+    					inputBorder='transparent'
+    					inputHoverColor='transparent'
+    				>
+  				    Sign in
+  				  </Button>
+  				</Link>
+  				<Link to={routes.signup()} className="auth">
+            <Button
+            inputHeight='2.5rem'
+            inputWidth='5rem'
+            inputColor='white'
+            inputBGColor='#FFC843'
+            inputBorder='#FFC843'
+            inputHoverColor='#F7D380'
+            >
+              Sign up
+            </Button>
+          </Link>
   			</Nav>
   		</Header>
   		<main>{children}</main>
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,16 +2,31 @@ import { Link, routes } from '@redwoodjs/router'
 import ContentLayout from 'src/layouts/ContentLayout'
 import styled from 'styled-components'
 import { Button } from 'src/styled'
+import placeholder from '../../../public/placeholder.png'
 
 const HomePage = () => {
 
-
-
   return (
     <ContentLayout>
-      <div className="banner">
-        <h1>Build new habits and celebrate the fails</h1>
-        <p>Microfails is the leading destination to find and create new habit building opportunities</p>
+      <div className='banner'>
+        <div className='banner-left'>
+          <h1>Celebrate the fails with others.</h1>
+          <p>Microfails is the leading destination try new things and fail with bravado</p>
+          <Link to={routes.signup()} className="auth" >
+            <Button
+              inputMargin='0'
+              inputHeight='2.5rem'
+              inputWidth='5rem'
+              inputColor='white'
+              inputBGColor='#FFC843'
+              inputBorder='2px solid #FFC843'
+              inputHoverColor='#F7D380'
+            >
+              Sign up
+            </Button>
+          </Link>       
+        </div>
+        <img src={placeholder} alt='skateboarder fall' style={{ width: '30rem', height: '30rem' }} />
       </div>
     </ContentLayout>
   )
