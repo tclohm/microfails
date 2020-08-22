08fed0153d05c0e59b31927cbe98e0ffea73f3a1
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-08-21 17:13:32 -0700

    edited user schema and added users to graphql and services

---
+++ api/src/graphql/users.sdl.js
@@ -0,0 +1,30 @@
+export const schema = gql`
+	type User {
+		id              Int!
+		profile         Profile?
+		email           String!
+		hashedPassword  String?
+		post            [post]!
+	}
+
+	type Query {
+		users: [User!]!
+		user(id: Int!): User!
+	}
+
+	input CreateUserInput {
+		email: String!
+		hashedPassword: String?
+	}
+
+	input UpdateUserInput {
+		email: String!
+		hashedPassword: String?
+	}
+
+	type Mutation {
+		createUser(input: CreateUserInput!): User!
+		updateUser(id: Int!, input: UpdateUserInput!): User!
+		deleteUser(id: Int!): User!
+	}
+`
